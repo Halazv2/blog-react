f226bae4c10569d378254f2b6ba3223aa3bbcb4d
Halazv2 <halaziani2003@gmail.com>
2022-06-26 13:49:08 +0100

    NavBar

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>
+                Mekna007's Blog
+            </h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/create">new blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
