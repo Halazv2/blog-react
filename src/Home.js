651da5fa7c2dede0c89be54aded57b3fcecfffd8
Halazv2 <halaziani2003@gmail.com>
2022-06-27 15:27:35 +0100

    working with props send data or function

--- src/Home.js
+++ src/Home.js
@@ -2,23 +2,64 @@ import HomeHero from "./HomeHero";
 import { useState } from "react";
 const Home = () => {
   const [name, setName] = useState("Mekna007");
-  const [age, setAge] = useState(21);
+  const [age, setAge] = useState(20);
   const title = "Welcome to my first React blog!";
-  const hobbies = ["basketball", "reading", "cars"];
+  const hobbies = ["Basketball", "Reading", "Cars"];
 
   const handdleClick = () => {
     setName("Mekna007");
     setAge(21);
   };
+
+  const [cars, setCars] = useState([
+    {
+      id : 1,
+      carName: "BMW",
+      type: "SUV",
+      price: "$1,000,000",
+      use: "Private",
+    },
+    {
+      id : 2,
+      carName: "Audi",
+      type: "SUV",
+      price: "$1,000,000",
+      use: "Private",
+    },
+    {
+      id : 3,
+      carName: "Mercedes",
+      type: "SUV",
+      price: "$1,000,000",
+      use: "Public",
+    },
+  ]);
+  const handdleDelete = (id) => {
+    const newCars = cars.filter((car) => car.id !== id);
+    setCars(newCars);
+  };
   return (
     <div className="content">
       <h1>{title}</h1>
-      <button onClick={handdleClick} style={{display:"none"}}>Click me</button>
+      <button onClick={handdleClick} style={{ display: "none" }}>
+        Click me
+      </button>
       <p>
-        {name} is {age} years old and likes {hobbies[0]}, {hobbies[1]} and{" "}
-        {hobbies[2]}
+        {name} was my Best friend he was {age} years old. he liked to play{" "}
+        {hobbies[0]} and {hobbies[1]}. he also loved {hobbies[2]} i made this
+        blog for him using React. <br />
+        <b>May he rest in peace ❤️</b>
       </p>
-        <HomeHero />
+      <HomeHero
+        cars={cars}
+        subtitle="Best cars for you!"
+        handdleDelete={handdleDelete}
+      />
+      <HomeHero
+        cars={cars.filter((car) => car.use === "Private")}
+        subtitle="Best Private cars"
+        handdleDelete={handdleDelete}
+      />
     </div>
   );
 };
--- src/HomeHero.js
+++ src/HomeHero.js
@@ -1,32 +1,24 @@
-import { useState } from "react";
-const HomeHero = () => {
-    const [cars, setCars] = useState([
-        {
-            carName: "BMW", type: "SUV", price: "$1,000,000",
-        },
-        {
-            carName: "Audi", type: "SUV", price: "$1,000,000",
-        },
-        {
-            carName: "Mercedes", type: "SUV", price: "$1,000,000",
-        }
-    ]);
-    return ( 
-        <div className="homeHero">
-            {
-                cars.map((car, index) => {
-                    return (
-                        <div className="blog-preview" key={index}>
-                            <h1>{car.carName}</h1>
-                            <p>{car.type}</p>
-                            <p>Price: {car.price}</p>
-                        </div>
-                    );
-                }
-                )
-            }
-        </div>
-     );
-}
- 
+const HomeHero = (/*props*/ { cars, subtitle,handdleDelete }) => {
+  //   const cars = props.cars;
+  //   const subtitle = props.subtitle;
+  return (
+    <div className="homeHero">
+      <h2>{subtitle}</h2>
+      {cars.map((car, id) => {
+        return (
+          <div className="blog-preview" key={id}>
+            <h1>{car.carName}</h1>
+            <p>{car.type}</p>
+            <p>Price: {car.price}</p>
+            <p>Price: {car.id}</p>
+            <button onClick={()=>handdleDelete(car.id)}>
+              Delete
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default HomeHero;
