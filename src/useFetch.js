b5d484562d68dd2580e294b764677cc871ede90c
Halazv2 <halaziani2003@gmail.com>
2022-06-28 11:11:26 +0100

    useEffect Cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,35 +1,34 @@
 import { useState, useEffect } from "react";
-const useFetch = (url)=>{
-    const [cars, setCars] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    useEffect(
-      () => {
-        setTimeout(() => {
-          fetch(url)
-            .then((res) => {
-              if (!res.ok) {
-                throw Error("Something went wrong");
-              }
-              return res.json();
-            })
-            .then((data) => {
-              setCars(data);
-              setLoading(false);
-              setError(null);
-            })
-            .catch((err) => {
-              setError(err);
-              setLoading(false);
-              setCars(null);
-            });
-        }, 1000);
-      },
-      [
-        /*name*/
-        url
-      ]
-    ); // the name is dependancy when it changes it will run the use effect
-    return {cars, loading, error};
-}
+const useFetch = (url) => {
+  const [cars, setCars] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const abortController = new AbortController();
+    setTimeout(() => {
+      fetch(url, { signal: abortController.signal })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Something went wrong");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setCars(data);
+          setLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("Fetching aborted"); 
+          } else {
+            setError(err.message);
+            setLoading(false);
+          }
+        });
+    }, 1000);
+    return () => abortController.abort();
+  }, [url]);
+  return { cars, loading, error };
+};
 export default useFetch;
