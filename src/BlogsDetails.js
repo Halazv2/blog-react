0f115e4c93cedce8999a23e3da15a02ac80006cf
Halazv2 <halaziani2003@gmail.com>
2022-06-29 12:59:56 +0100

    send data by making post request

--- src/BlogsDetails.js
+++ src/BlogsDetails.js
@@ -5,11 +5,11 @@ const BlogDetails = () => {
   const { cars, loading, error } = useFetch(`http://localhost:8000/cars/${id}`);
   return (
     <div className="blog-details">
-      <h1>Blog Details - {id} </h1>
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       {cars && (
         <article>
+          <h1>Blog Details - {id} </h1>
           <h1>{cars.Name}</h1>
           <p>{cars.Origin}</p>
           <p>Year: {cars.Year}</p>
--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,71 @@
+import { useState } from "react";
 const Create = () => {
-    return ( 
-        <div className="create">
-            <h3>Add new car</h3>
-        </div>
-     );
-}
- 
+  const [Name, setName] = useState("");
+  const [Origin, setOrigin] = useState("");
+  const [Year, setYear] = useState("");
+  const [Description, setDescription] = useState("");
+  const [isPanding, setPanding] = useState(false);
+  const empty = () => {
+    setName("");
+    setOrigin("");
+    setYear("");
+    setDescription("");
+  };
+  const handeleSubmit = (e) => {
+    e.preventDefault();
+    setPanding(true);
+    const newCar = { Name, Origin, Year, Description };
+    fetch("http://localhost:8000/cars", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCar),
+    }).then(() => {
+      setTimeout(() => {
+        setPanding(false);
+      }, 1000);
+      empty();
+    });
+  };
+  return (
+    <div className="create">
+      <h3>Create new blog</h3>
+      <form onSubmit={handeleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={Name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Origin:</label>
+        <input
+          type="text"
+          name="Origin"
+          value={Origin}
+          onChange={(e) => setOrigin(e.target.value)}
+        />
+        <label>Year:</label>
+        <input
+          type="date"
+          name="Year"
+          value={Year}
+          onChange={(e) => setYear(e.target.value)}
+        />
+        <label>Description:</label>
+        <textarea
+          required
+          value={Description}
+          onChange={(e) => setDescription(e.target.value)}
+        >
+          {" "}
+        </textarea>
+        {!isPanding && <button type="submit">Create</button>}
+        {isPanding && <button type="submit">Adding blog..</button>}
+      </form>
+    </div>
+  );
+};
+
 export default Create;
